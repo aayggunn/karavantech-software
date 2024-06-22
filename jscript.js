c586d6486a484acbf53be72422a3f5a07e82b58d
Abdurrahman Aygun <139715066+aayggunn@users.noreply.github.com>
2024-06-23 01:01:09 +0300

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -1,13 +1,24 @@
-$(document).ready(function(){
-	$('#book_pick_date, #book_off_date, #tarih3').datepicker({});
-	
-	$(window).scroll(function(){
-		$('.bolum2, .bolum3, .bolum4, .bolum5, .bolum6, .bolum7, footer').each(function(){
-			var ustKenar = $(this).offset().top;
-			var pencereAlt = $(window).scrollTop()+$(window).height();
-			if(pencereAlt > ustKenar){
-				$(this).animate({'opacity':'1'},1000);
-			}
-		})
-	})
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a.nav-link").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
 });
